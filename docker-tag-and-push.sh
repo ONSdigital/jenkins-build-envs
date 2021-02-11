3cb407a09b400ece6205ba0e24684373d3090fae
Ash Jindal <ajorpheus@users.noreply.github.com>
2021-02-11 20:01:15 +0000

    Add node v15.3.0 and corresponding CM image
    Refactor cm Dockerfile to accept a configurable parent version

--- docker-tag-and-push.sh
+++ docker-tag-and-push.sh
@@ -4,20 +4,20 @@ tags=$1
 imageid=$2
 reponame=$3
 
-for aTag in $(echo $tags | sed "s/,/ /g")
+for aTag in $(echo "$tags" | sed "s/,/ /g")
 do
-    # call your procedure/other scripts here below
-    printf "\n\nTagging $imageid with $aTag\n"
+    printf "%b" "\n\nTagging $imageid with $aTag\n"
     docker tag "$imageid" "onsdigital/$reponame:$aTag"
     docker tag "$imageid" "ashjindal/$reponame:$aTag"
-    # docker tag "$imageid" "arti:8081/onsdigital/$reponame:$aTag"
 done
 
-# docker tag ac508c28f0f6 onsdigital/jenkins-slave-cm 
- # docker tag ac508c28f0f6 ashjindal/jenkins-slave-cm 
- # docker tag ac508c28f0f6 arti:8081/onsdigital/jenkins-slave-cm
+for aTag in $(echo "$tags" | sed "s/,/ /g")
+do
+    printf "%b" "\n\nPushing $imageid to onsdigital/$reponame:$aTag \n"
+    docker push  "onsdigital/$reponame:$aTag"
 
-printf "\n\nPushing $imageid to $reponame\n"
-docker push -a "onsdigital/$reponame"
-docker push -a "ashjindal/$reponame"
-# docker push -a "arti:8081/onsdigital/$reponame"
+    printf "%b" "\n\nPushing $imageid to ashjindal/$reponame:$aTag\n"
+    docker push  "ashjindal/$reponame:$aTag"
+done
+
+
