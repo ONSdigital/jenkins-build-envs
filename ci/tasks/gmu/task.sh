a8ca6986d4c2d660a47311ea3a103c17b2656461
Nik Webb <nik.webb@ons.gov.uk>
2023-01-09 21:25:54 +0000

    updating naming

--- ci/tasks/gmu/task.sh
+++ ci/tasks/gmu/task.sh
@@ -4,11 +4,6 @@ aws s3 cp $S3_BUCKET/$CERT_FOLDER ./certs --recursive
 
 aws s3 cp $S3_BUCKET/$GMU_FOLDER ./gmudownload
 
-ls
-
-ls certs
+cp -R gmudownload certs gmu
 
 ls gmu
-
-cp -R gmudownload gmu
-cp -R certs gmu
